b227d1f8275d3ed62a89b4db76847aa6dbfcdba3
Aghalar8330 <agalaryusublu8330@gmail.com>
2021-06-12 10:33:32 +0400

    Js tasks added

--- JsTasks/Task01/3.js
+++ JsTasks/Task01/3.js
@@ -1,4 +1,3 @@
-
 // Dəyərin tipini rəqəmə çevirərək əldə olunan nəticənin 2 qatını ekrana yazdırın.Tip çevirmə uğursuz alınarsa ona uyğun olaraq error mesajı çap elətdirin
 
 let b = prompt("");
@@ -7,10 +6,10 @@ let deyer = Number(b);
 // console.log(deyer);
 // console.log(typeof(deyer));
 
-if( isNaN(deyer)){
-  console.log( deyer + " Ugursuz oldu");
-}else{
-  console.log( deyer + " Ugurlu oldu");
+if (isNaN(deyer)) {
+    console.log(deyer + " Ugursuz oldu");
+} else {
+    console.log(deyer * 2 + " Ugurlu oldu");
 }
 
 // Ternary Operatorla
