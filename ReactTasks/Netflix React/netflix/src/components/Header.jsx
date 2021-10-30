3f02008451df0972b9a36682a7e0489251826f03
Aghalar <agalaryusublu8330@gmail.com>
2021-10-30 21:59:22 +0400

    react newsPaper task added

--- ReactTasks/Netflix React/netflix/src/components/Header.jsx
+++ ReactTasks/Netflix React/netflix/src/components/Header.jsx
@@ -1,39 +1,36 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom';
-import { useEffect, useState, useCallback } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState, useMemo, useCallback } from 'react';
 import logo from '../images/logo.png'
 
 const Header = () => {
-
+    let location = useLocation()
     const [profile, setProfile] = useState('')
 
     const LogOut = useCallback(() => {
         if (profile !== null) {
             return <span onClick={removeProfile} className="btn-logout btn">Log Out  <i class="ms-1 fas fa-sign-out-alt"></i></span>
         }
-       
     }, [profile])
 
     const removeProfile = useCallback(() => {
-        window.localStorage.removeItem("firstName");
-    }, [])
+        window.localStorage.removeItem("firstName")
+    },[]);
 
     useEffect(() => {
         const profileName = localStorage.getItem("firstName");
         setProfile(profileName)
-    }, [profile])
-
-
+    }, [profile, location.pathname])
 
 
     return (
         <div className='header'>
             <Link to="/"> <img src={logo} alt="" style={{ width: '92px' }} /></Link>
-           <div className="profile">
-           <Link to={profile == null ? `/register` : `/`} className='register-link'> {profile == null ? "Register" : `Hello ${profile}`}</Link>
-            {LogOut()}
-           </div>
+            <div className="profile">
+                <Link to={profile == null ? `/register` : `/`} className='register-link'> {profile == null ? "Register" : `Hello ${profile}`}</Link>
+                {LogOut()}
+            </div>
         </div>
 
     )
---
+++ ReactTasks/React Newspaper Api/app/src/App.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { useCallback, useRef, useEffect, useState } from 'react';
+import shortid from "short-id";
+
+import './App.css';
+
+
+function App() {
+  const inputValue = useRef(null)
+  const pageLink = useRef(null)
+  const [data, setData] = useState()
+  const [pageCount, setPageCount] = useState()
+
+
+  const [terms, setTerms] = useState()
+  const [page, setPage] = useState(1)
+
+
+  useEffect(() => {
+    fetch(`https://chroniclingamerica.loc.gov/search/titles/results/?terms=${terms}&format=json&page=${page}`).then(a => a.json())
+      .then((data) => {
+        setData(data);
+        setPageCount(data.totalItems / (data.endIndex - data.startIndex));
+      }).catch(() => {
+        alert("data yoxdur")
+      })
+  }, [terms, page, pageCount])
+
+  const Search = (e) => {
+    e.preventDefault()
+    setTerms(inputValue.current.value)
+  }
+
+  const pageNum = []
+  for (let i = 1; i <= pageCount; i++) {
+    pageNum.push(i)
+  }
+
+  return <>
+    <div className="main mt-5">
+      <div className="container">
+        <div className="row offset-4">
+          <div className="col-lg-6">
+            <form>
+              <div class="form-row text-center">
+                <div class="col">
+                  <input type="text" ref={inputValue} class="form-control" placeholder="Search..." />
+                  <button className='btn btn-success mt-3' onClick={Search}> Search</button>
+                </div>
+              </div>
+            </form>
+            <nav aria-label="Page navigation example " className='mt-5 d-flex justify-content-center'>
+              <ul class="pagination">
+                {
+                  pageNum.map((num) => { 
+                    return <li key={shortid.generate()} class="page-item"><a class="page-link" href="#" ref={pageLink} onClick={() => setPage(num)}>{num}</a></li>
+                  })
+                }
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="data-show d-flex justify-content-between">
+          <div className="title">
+            <h4>Title</h4>
+            {
+              data?.items.map((elem)  => {
+                return <p key={shortid.generate()} >{elem.title}</p>
+              })
+            }
+          </div>
+          <div className="start-year">
+            <h4>Start Year</h4>
+            {
+              data?.items.map((elem) => {
+                return <p key={shortid.generate()} >{elem.start_year}</p>
+              })
+            }
+          </div>
+          <div className="publisher">
+            <h4>Publisher</h4>
+            {
+              data?.items.map((elem) => {
+                return <p key={shortid.generate()}>{elem.publisher}</p>
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+}
+
+export default App;
